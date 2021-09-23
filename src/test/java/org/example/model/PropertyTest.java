43a294357aa19dbf7aa2d50810804e90d3186237
Roman <WizardPronomuos@yandex.ru>
2021-09-23 20:09:20 +0300

    fix: tests

--- src/test/java/org/example/model/PropertyTest.java
+++ src/test/java/org/example/model/PropertyTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 
 public class PropertyTest {
     @Test
-    public void createProperty_WhenDouble_ReturnDoubleProperty() throws InvalidPropertyException {
-        assertEquals(1.5, Property.createProperty("test", "1.5").getValue());
-        assertEquals(10.1, Property.createProperty("test", "10.1").getValue());
+    public void createProperty_WhenInt_ReturnIntProperty() throws InvalidPropertyException {
+        assertEquals(1, Property.createProperty("test", "1").getValue());
+        assertEquals(10, Property.createProperty("test", "10").getValue());
     }
 }
