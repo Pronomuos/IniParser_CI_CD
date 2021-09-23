fe2cc7a0584f66ed637fd6f87f68a6dc8236a973
Roman <WizardPronomuos@yandex.ru>
2021-09-23 20:08:49 +0300

    fix: tests

--- src/test/java/org/example/model/PropertyTest.java
+++ src/test/java/org/example/model/PropertyTest.java
@@ -11,17 +11,4 @@ public class PropertyTest {
         assertEquals(1.5, Property.createProperty("test", "1.5").getValue());
         assertEquals(10.1, Property.createProperty("test", "10.1").getValue());
     }
-
-    @Test
-    public void createProperty_WhenInt_ReturnIntProperty() throws InvalidPropertyException {
-        assertEquals(1, Property.createProperty("test", "1").getValue());
-        assertEquals(10, Property.createProperty("test", "10").getValue());
-    }
-
-    @Test
-    public void createProperty_WhenString_ReturnStrProperty() throws InvalidPropertyException {
-        assertEquals("str", Property.createProperty("test", "str").getValue());
-        assertEquals("STR", Property.createProperty("test", "STR").getValue());
-
-    }
 }
