3499ae9958c6a962e4f6854ac288b77c6ff1e144
Roman <WizardPronomuos@yandex.ru>
2021-09-23 20:20:58 +0300

    update: tests

--- src/test/java/org/example/model/PropertyTest.java
+++ src/test/java/org/example/model/PropertyTest.java
@@ -9,6 +9,6 @@ public class PropertyTest {
     @Test
     public void createProperty_WhenInt_ReturnIntProperty() throws InvalidPropertyException {
         assertEquals(1, Property.createProperty("test", "1").getValue());
-        assertEquals(10, Property.createProperty("test", "10").getValue());
+        assertEquals(100, Property.createProperty("test", "100").getValue());
     }
 }
