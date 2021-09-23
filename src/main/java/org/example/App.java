06a166ccc410e96930abe3931f554a95861c8cf5
Roman <WizardPronomuos@yandex.ru>
2021-09-23 20:05:42 +0300

    add: app and parser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,40 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.example.exception.InvalidTypeConversion;
+import org.example.exception.NotFoundPropertyException;
+import org.example.exception.NotFoundSectionException;
+import org.example.exception.ParserException;
+import org.example.model.ParsedIniFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        if (args.length < 3 || args.length > 4) {
+            System.err.println("Invalid args count");
+            return;
+        }
+        try {
+            ParsedIniFile iniFile = IniParser.parse(args[0]);
+            switch (args[3]) {
+                case "int":
+                    System.out.println(
+                            iniFile.getSection(args[1])
+                                    .getInt(args[2])
+                    );
+                    break;
+                default:
+                    System.err.println("Invalid type");
+            }
+        } catch (NotFoundSectionException ex) {
+            System.err.println("File not found");
+        } catch (NotFoundPropertyException | InvalidTypeConversion ex) {
+            System.err.println("Parsing error");
+            ex.printStackTrace();
+        } catch (ParserException e) {
+            e.printStackTrace();
+        }
     }
 }
